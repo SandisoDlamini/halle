b8ae088aa92f371f236f519e531a326c766f4b11
SandisoDlamini <sihlegamer65@gmail.com>
2024-05-06 15:26:59 +0200

    intergrated tailwind and templates perfectly.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ use axum::{
     routing::get,
     Router,
 };
-use tokio::net::TcpListener;
-use lazy_static::{self, lazy_static};
+use lazy_static::lazy_static;
 use tera::Tera;
+use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
+use tracing::{debug, info};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 lazy_static! {
     pub static ref TEMPLATES: Tera = {
-        let mut tera = match Tera::new("examples/basic/templates/**/*") {
+        let mut tera = match Tera::new("templates/**/*") {
             Ok(t) => t,
             Err(e) => {
                 println!("Parsing error(s): {}", e);
@@ -17,18 +20,40 @@ lazy_static! {
             }
         };
         tera.autoescape_on(vec![".html", ".sql"]);
-        tera.register_filter("do_nothing", do_nothing_filter);
         tera
     };
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(hello_handler));
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "axum_webapp=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    info!("initializing router...");
+
+    // region: -- create router
+    let assets_path = std::env::current_dir().unwrap();
+    info!("loading assets from {:?}", &assets_path);
+    let app = Router::new()
+        .route("/", get(hello_handler))
+        .route("/main-page", get(hello_handler2))
+        .nest_service(
+            "/assets",
+            ServeDir::new(format!("{}/assets", assets_path.to_str().unwrap())),
+        );
+    // region: -- create router
 
     // region: ---Start Server
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
-    println!("->> LISTENING on {:?}\n", listener.local_addr().unwrap());
+    info!(
+        "router initialized, now LISTENING on {:?}\n",
+        listener.local_addr().unwrap()
+    );
     axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
@@ -37,8 +62,18 @@ async fn main() {
 
 // Region: ---HANDLER functions
 async fn hello_handler() -> impl IntoResponse {
-    println!("->> {:<12} - app - hello_handler", "HANDLER");
+    debug!("{:<12} - app - hello_handler", "HANDLER");
 
-    Html("Hello Sphe")
+    let context1 = tera::Context::new();
+    let page_content = TEMPLATES.render("index.html", &context1).unwrap();
+    Html(page_content)
+}
+
+async fn hello_handler2() -> impl IntoResponse {
+    debug!("{:<12} - app - hello_handler2", "HANDLER");
+
+    let context1 = tera::Context::new();
+    let page_content = TEMPLATES.render("main-page.html", &context1).unwrap();
+    Html(page_content)
 }
 // Region: --- HANDLER functions
