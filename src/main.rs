c1f15a9958357512cd43398c804955fb68e13ee5
SandisoDlamini <sihlegamer65@gmail.com>
2024-07-13 20:52:32 +0200

    added additional tracinig and logging.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use std::sync::Arc;
 
-use axum::{routing::get, Router};
+use axum::{
+    body::Bytes,
+    extract::MatchedPath,
+    http::{HeaderMap, Request},
+    response::Response,
+    routing::get,
+    Router,
+};
 use sqlx::{postgres::PgPoolOptions, PgPool};
+use std::time::Duration;
 use tokio::net::TcpListener;
-use tower_http::services::ServeDir;
-use tracing::{debug, info};
+use tower_http::{classify::ServerErrorsFailureClass, services::ServeDir, trace::TraceLayer};
+use tracing::{debug, info, info_span, Span};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod web;
-
-const POSTGRES_URL: &str = env!("POSTGRES_URL");
 
 struct AppState {
     conn: PgPool,
@@ -25,9 +31,12 @@ struct Article {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let db_connection_str = std::env::var("DATABASE_URL")
+        .unwrap_or_else(|_| "postgres://postgres:password@localhost".to_string());
+
     let conn = PgPoolOptions::new()
         .max_connections(5)
-        .connect(POSTGRES_URL)
+        .connect(&db_connection_str)
         .await?;
 
     sqlx::migrate!("./migrations").run(&conn).await?;
@@ -44,8 +53,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     tracing_subscriber::registry()
         .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "axum_webapp=debug".into()),
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                "axum_webapp=debug,tower_http=debug,axum::rejection=trace".into()
+            }),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
@@ -62,6 +72,45 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let app = Router::new()
         .nest("/api", api_router)
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &Request<_>| {
+                    // Log the matched route's path (with placeholders not filled in).
+                    // Use request.uri() or OriginalUri if you want the real path.
+                    let matched_path = request
+                        .extensions()
+                        .get::<MatchedPath>()
+                        .map(MatchedPath::as_str);
+
+                    info_span!(
+                        "http_request",
+                        method = ?request.method(),
+                        matched_path,
+                        some_other_field = tracing::field::Empty,
+                    )
+                })
+                .on_request(|_request: &Request<_>, _span: &Span| {
+                    // You can use `_span.record("some_other_field", value)` in one of these
+                    // closures to attach a value to the initially empty field in the info_span
+                    // created above.
+                })
+                .on_response(|_response: &Response, _latency: Duration, _span: &Span| {
+                    // ...
+                })
+                .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {
+                    // ...
+                })
+                .on_eos(
+                    |_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {
+                        // ...
+                    },
+                )
+                .on_failure(
+                    |_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
+                        // ...
+                    },
+                ),
+        )
         .with_state(Arc::new(state))
         .merge(web::routes_handler::routes())
         .nest_service(
